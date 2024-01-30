6dc1f79137bab315a5433c2731a8157346f3d554
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-01-30 18:46:48 +0530

    Styln added to the proect

--- pages/Component/ProductPage.tsx
+++ pages/Component/ProductPage.tsx
@@ -1,5 +1,7 @@
 "use client";
-import { HiX } from "react-icons/hi";
+import { FcInfo } from "react-icons/fc";
+import { IoClose } from "react-icons/io5";
+import { RiDeleteBin6Line } from "react-icons/ri";
 import React from "react";
 import style from "@/styles/style.module.css";
 import {
@@ -9,6 +11,7 @@ import {
   Textarea,
   Checkbox,
   FileInput,
+  Button,
 } from "flowbite-react";
 
 export function ProductPage() {
@@ -19,7 +22,7 @@ export function ProductPage() {
           <div className={style.text}>Add Product</div>
         </div>
         <div className={style.close_btn}>
-          <HiX className={style.close} />
+          <IoClose className={style.close} />
         </div>
       </div>
       <div className={style.modal_body}>
@@ -35,8 +38,9 @@ export function ProductPage() {
             />
           </div>
           <div className={style.select}>
-            <div>
+            <div className={style.info}>
               <Label value="Select Language" />
+              <FcInfo className={style.info_img} />
             </div>
             <Select id="countries" required className={style.select_opt}>
               <option>Electronics</option>
@@ -115,7 +119,19 @@ export function ProductPage() {
 
         <div className={style.description}>
           <Label htmlFor="comment" value="Description" />
-          <Textarea id="comment" className={style.textArea} />
+          <Textarea
+            id="comment"
+            placeholder="Standard glass,
+            3.8GHz 8-core 10th-generation Intel Core i7 processor,
+            Turbo Boost up to 5.0GHz,
+            16GB 2666MHz DDR4 memory,
+            Radeon Pro 5500 XT with 8GB of GDDR6 memory,
+            256GB SSD storage,
+            Gigabit Ethernet,
+            Magic Mouse 2,
+            Magic Keyboard-Us"
+            className={style.textArea}
+          />
         </div>
 
         <div className={style.selling_info}>
@@ -137,19 +153,16 @@ export function ProductPage() {
         </div>
 
         <div className={style.image_conatiner}>
-          <Label value="Product Images" />
-          <div className={style.productImages}></div>
+          <Label className={style.img_prd_name} value="Product Images" />
+          <div className={style.product_Images}></div>
         </div>
 
-        <div className={style.eigthContainer}>
-          <div className="flex w-full items-center justify-center">
-            <Label
-              htmlFor="dropzone-file"
-              className="dark:hover:bg-bray-800 flex h-64 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
-            >
-              <div className="flex flex-col items-center justify-center pb-6 pt-5">
+        <div className={style.dropdown_container}>
+          <div className={style.dropdown}>
+            <Label htmlFor="dropzone-file" className={style.dropzone}>
+              <div className={style.drop_style}>
                 <svg
-                  className="mb-4 h-8 w-8 text-gray-500 dark:text-gray-400"
+                  className={style.svg}
                   aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -163,17 +176,31 @@ export function ProductPage() {
                     d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
                   />
                 </svg>
-                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                  <span className="font-semibold">Click to upload</span> or drag
+                <p className={style.desc}>
+                  <span className={style.bold}>Click to upload</span> or drag
                   and drop
                 </p>
-                <p className="text-xs text-gray-500 dark:text-gray-400">
+                <p className={style.img_type}>
                   SVG, PNG, JPG or GIF (MAX. 800x400px)
                 </p>
               </div>
-              <FileInput id="dropzone-file" className="hidden" />
+              <FileInput
+                id="dropzone-file"
+                accept="image/*"
+                className="hidden"
+              />
             </Label>
           </div>
+        </div>
+        <div className={style.submit_btn}>
+          <Button className={style.sub_btn} type="submit">
+            Update Product
+          </Button>
+          <Button className={style.del_btn}>
+            <RiDeleteBin6Line />
+            <span />
+            Delete
+          </Button>
         </div>
       </div>
     </div>
