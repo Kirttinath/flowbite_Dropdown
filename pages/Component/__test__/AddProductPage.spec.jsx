bdf20bcefda52c0887ebc7d005926233c9d1374b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-09 12:54:57 +0530

    tests for rendering with addbutton click fire event

--- pages/Component/__test__/AddProductPage.spec.jsx
+++ pages/Component/__test__/AddProductPage.spec.jsx
@@ -1,8 +1,18 @@
 import AddProductPage from "../AddProductPage";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("AddProductPage Test", () => {
   test("Rendering Test", () => {
     render(<AddProductPage />);
+  });
+  test("Addproduct Button Click", () => {
+    render(<AddProductPage />);
+    const button = screen.getByText("Add Product");
+    fireEvent.click(button);
+    expect(screen.getByLabelText("Product Name :")).toBeInTheDocument();
+    expect(screen.getByLabelText("Select Type :")).toBeInTheDocument();
+    expect(screen.getByLabelText("Brand")).toBeInTheDocument();
+    expect(screen.getByLabelText("Price")).toBeInTheDocument();
+    expect(screen.getByLabelText("Description")).toBeInTheDocument();
   });
 });
