473cc9dd63a5640fa057f2aeab7bc6ec4af60b8b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-09 12:42:50 +0530

    environment setup with nextjs of react-testing-library with jest

--- pages/Component/__test__/AddProductPage.spec.jsx
+++ pages/Component/__test__/AddProductPage.spec.jsx
@@ -1,2 +1,8 @@
 import AddProductPage from "../AddProductPage";
-import {render, screen} from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
+
+describe("AddProductPage Test", () => {
+  test("Rendering Test", () => {
+    render(<AddProductPage />);
+  });
+});
