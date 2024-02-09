e2267f6a84d44d3f0d434787f1331907f178a84a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-09 14:39:18 +0530

    buttons in the page testing complete

--- pages/Component/__test__/AddProductPage.spec.jsx
+++ pages/Component/__test__/AddProductPage.spec.jsx
@@ -5,6 +5,20 @@ describe("AddProductPage Test", () => {
   test("Rendering Test", () => {
     render(<AddProductPage />);
   });
+  test("Buttons In The Page", () => {
+    render(<AddProductPage />);
+    const buttons = screen.getAllByRole("button");
+    for (let i = 0; i < buttons.length; i++) {
+      expect(buttons[i]).toBeInTheDocument();
+    }
+  });
+  test("Specify the Buttons In the page", () => {
+    render(<AddProductPage />);
+    const reset = screen.getByText("Reset All");
+    const submit = screen.getByText("Submit");
+    expect(reset).toBeInTheDocument();
+    expect(submit).toBeInTheDocument();
+  });
   test("Addproduct Button Click", () => {
     render(<AddProductPage />);
     const button = screen.getByText("Add Product");
