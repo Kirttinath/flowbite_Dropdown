2c38fc7d1b8a441e7868862095458133e998873e
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-03-21 10:54:52 +0530

    automatic release build in main

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 // import { ProductPage } from "./Component/ProductPage";
 import AddProductPage from "./Component/AddProductPage";
+import { ProductPage } from "./Component/ProductPage";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <AddProductPage />
-    /* <ProductPage /> */
+    // <AddProductPage />
+    <ProductPage />
   );
 }
