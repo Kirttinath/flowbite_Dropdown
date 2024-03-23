9aee00cdc50ee375b4154a69137c05e9de5fb136
Kirttinath <kirttinathojha000@gmail.com>
2024-03-23 21:47:03 +0530

    feat: index page updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+// import Image from "next/image";
 import { Inter } from "next/font/google";
 // import { ProductPage } from "./Component/ProductPage";
-import AddProductPage from "./Component/AddProductPage";
-import { ProductPage } from "./Component/ProductPage";
+// import AddProductPage from "./Component/AddProductPage";
+import ProductPage from "./Component/ProductPage";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
