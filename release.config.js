61369e6223ba7f0c2d0afbe0e288627cf13780ec
Kirttinath <kirttinathojha000@gmail.com>
2024-03-25 11:30:33 +0530

    BREAKING CHNAGE: main

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 const config = {
-  branches: ["main"],
+  branches: ["main", { name: "dev", prerelease: true }],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
